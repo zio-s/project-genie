e50eb0e4593c6f5a0449d86149a5821bb7601556
seming2 <popqr3@naver.com>
2024-10-24 13:59:44 +0900

    album 섹션 스크롤 트리고 효과 구현, 해당 아이템 진입시 배경 교체

--- js/gsap.js
+++ js/gsap.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
   // Fullpage.js 초기화
@@ -8,13 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
     easingcss3: "cubic-bezier(0.76, 0, 0.24, 1)",
     scrollingSpeed: 1250,
     touchSensitivity: 0.01,
+    // normalScrollElements: '.album-list-wrapper', // sec-album에서만 fullpage 스크롤 해제
     afterLoad: (origin, destination) => {
       if (destination.index === 0) {
         ScrollTrigger.refresh();
       }
     },
   });
-
   const bigLogo = document.querySelector(".big-logo");
   const smallLogo = document.querySelector(".small-logo");
   const heightBox = document.querySelector(".height_box");
@@ -83,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // 비디오 초기 상태 설정
   gsap.set(video, {
     scale: 0.35,
-    y: "25%",
+    y: "65%",
     borderRadius: 35,
   });
   ScrollTrigger.create({
@@ -91,14 +92,14 @@ document.addEventListener("DOMContentLoaded", () => {
     scroller: secScroller, // 스크롤러로 사용할 요소
     start: "top 50%",
     end: "bottom bottom",
-    scrub: 4,
+    scrub: 5,
     markers: false,
     // 애니메이션 설정
     animation: gsap.fromTo(
       video,
       {
         scale: 0.35,
-        y: "25%",
+        y: "65%",
         borderRadius: 35,
       },
       {
@@ -111,23 +112,70 @@ document.addEventListener("DOMContentLoaded", () => {
     ),
   });
   ScrollTrigger.refresh();
+  
+// 모든 앨범 아이템 배열화
+const albumItems = gsap.utils.toArray(".album-item");
+const albumS = document.querySelector(".sec-album .fp-overflow");
+const albumBg = document.querySelector(".album-bg");  
+let previousSrc = [albumBg.style.backgroundImage];
+// 각 앨범 아이템에 대해 반복문 실행
+albumItems.forEach((item, index) => {
+  const img = item.querySelector(".album-cover");
+  const imgSrc = img.getAttribute("src");   
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: item,  // 앨범 리스트가 트리거
+      scroller: albumS,
+      start: "top 100%",        // 앨범 리스트가 80%에 도달할 때 시작
+      end: "top 80%",       // 앨범 리스트가 20%에 도달할 때 종료
+      scrub: 7, 
+      onEnter: () => {
+        // 새로운 배경 이미지 설정
+        if (previousSrc.length === 0) {
+          previousSrc.push(albumBg.style.backgroundImage); // 처음 배경 이미지 저장
+          
+        } else {
+          previousSrc[index] = albumBg.style.backgroundImage; // 현재 배경 이미지 저장
+        }
+        albumBg.style.backgroundImage = `url(${imgSrc})`; 
+      },
+      onLeaveBack: () => {
+        // 스크롤이 뒤로 돌아갈 때 이전 이미지 복원
+        if (previousSrc[index]) {
+          albumBg.style.backgroundImage = previousSrc[index]; // 이전 이미지로 복원
+        } else {
+          albumBg.style.backgroundImage = previousSrc[index]; // 이전 이미지가 없으면 none으로 설정
+        }
+      },
+      onLeave: () => {
+        // 스크롤이 아래로 갈 때 다음 이미지 복원
+        if (previousSrc[index + 1]) {
+          albumBg.style.backgroundImage = previousSrc[index + 1] || "none"; // 다음 이미지로 복원
+        }
+      },
+    },
+  });
 
-  // 앨범 스크롤 이벤트
-  // const albumList = document.querySelector(".album-list"); // 앨범 리스트
-  // const wrapper = document.querySelector(".album-container"); // 래퍼
+  // 애니메이션 정의
+  tl.fromTo(
+    item,
+    {  y: 50, scale:0.2,  },    // 시작 상태 (아래에서 투명하게 시작)
+    {  y: 0, scale:1, duration: .5, ease: "cubic-bezier(0.45, 0, 0.55, 1)" } // 최종 상태 (보임)
+  );
+});
 
-  // ScrollTrigger.create({
-  //   trigger: albumList, // 스크롤 트리거를 앨범 리스트에 걸기
-  //   scroller: wrapper,  // 앨범 리스트를 스크롤 할 요소
-  //   start: "top top",   // 시작 지점
-  //   end: () => `${albumList.scrollHeight - wrapper.clientHeight}px`, // 스크롤 종료 지점
-  //   scrub: 4,        // 스크롤에 따라 부드럽게 애니메이션
-  //   pin: false,         // 고정된 타이틀을 위한 핀 설정 X
 
-  // });
-
-    window.addEventListener("resize", () => {
+  window.addEventListener("resize", () => {
     const introSection = document.querySelector(".intro-sec");
     introSection.style.height = `${window.innerHeight}px`;
   });
+
+
+
 });
+
+
+
+
+
+
