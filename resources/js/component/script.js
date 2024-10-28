042b3f358ea196785241b2c688a88eae8d9b1fb8
seming2 <popqr3@naver.com>
2024-10-28 10:34:18 +0900

    뎁스 정리

--- resources/js/component/script.js
+++ resources/js/component/script.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const topTxt = document.querySelector(".top-t");
+  const bottomTxt = document.querySelector(".bottom-t");
 
-const topTxt = document.querySelector('.top-t');
-const bottomTxt = document.querySelector('.bottom-t');
+  // 공통 함수: 마우스 오버 시 active 클래스 추가
+  function addActiveClass(element) {
+    element.classList.add("active");
+  }
 
-// 공통 함수: 마우스 오버 시 active 클래스 추가
-function addActiveClass(element) {
-  element.classList.add('active');
-}
+  // 공통 함수: 마우스가 벗어났을 때 active 클래스 제거
+  function removeActiveClass(element) {
+    element.classList.remove("active");
+  }
 
-// 공통 함수: 마우스가 벗어났을 때 active 클래스 제거
-function removeActiveClass(element) {
-  element.classList.remove('active');
-}
+  // 이벤트 리스너 추가 (topTxt)
+  topTxt.addEventListener("mouseenter", () => addActiveClass(topTxt));
+  topTxt.addEventListener("mouseleave", () => removeActiveClass(topTxt));
 
-// 이벤트 리스너 추가 (topTxt)
-topTxt.addEventListener('mouseenter', () => addActiveClass(topTxt));
-topTxt.addEventListener('mouseleave', () => removeActiveClass(topTxt));
-
-// 이벤트 리스너 추가 (bottomTxt)
-bottomTxt.addEventListener('mouseenter', () => addActiveClass(bottomTxt));
-bottomTxt.addEventListener('mouseleave', () => removeActiveClass(bottomTxt));
-
-
-
+  // 이벤트 리스너 추가 (bottomTxt)
+  bottomTxt.addEventListener("mouseenter", () => addActiveClass(bottomTxt));
+  bottomTxt.addEventListener("mouseleave", () => removeActiveClass(bottomTxt));
 });
