a25a46ff9c5d23f7bde9fbd36ed1e5d5f9fc32b4
seming2 <popqr3@naver.com>
2025-01-29 14:32:03 +0900

    주석정리

--- resources/js/component/script.js
+++ resources/js/component/script.js
@@ -1,68 +1,62 @@
 window.onload = function () {
   setTimeout(() => {
-    const loadingSpinner = document.querySelector(".loading");
+    const loadingSpinner = document.querySelector('.loading');
     // const content = document.getElementById("content");
 
-    loadingSpinner.classList.add("hidden"); // 스피너의 점진적 사라짐
-    loadingSpinner.style.display = "none";
+    loadingSpinner.classList.add('hidden');
+    loadingSpinner.style.display = 'none';
     // content.style.display = "block"; // 콘텐츠 표시
   }, 1000); // 1초 딜레이
 };
-const topTxt = document.querySelector(".top-t");
-const bottomTxt = document.querySelector(".bottom-t");
-const menuBtn = document.querySelector(".menu-btn");
+const topTxt = document.querySelector('.top-t');
+const bottomTxt = document.querySelector('.bottom-t');
+const menuBtn = document.querySelector('.menu-btn');
 
-document.querySelectorAll("#gnb .gnb__depth1 li a").forEach((anchor) => {
-  anchor.addEventListener("click", function (event) {
-    document.querySelectorAll("#gnb .gnb__depth1 li a").forEach((a) => a.classList.remove("active"));
+document.querySelectorAll('#gnb .gnb__depth1 li a').forEach((anchor) => {
+  anchor.addEventListener('click', function (event) {
+    document.querySelectorAll('#gnb .gnb__depth1 li a').forEach((a) => a.classList.remove('active'));
 
-    event.currentTarget.classList.add("active");
+    event.currentTarget.classList.add('active');
   });
 });
 
-// 공통 함수: 마우스 오버 시 active 클래스 추가
-function addActiveClass(element) {
-  element.classList.add("active");
+function addActiveClass(e) {
+  e.classList.add('active');
 }
 
-// 공통 함수: 마우스가 벗어났을 때 active 클래스 제거
-function removeActiveClass(element) {
-  element.classList.remove("active");
+function removeActiveClass(e) {
+  e.classList.remove('active');
 }
 
-// topTxt가 있을 때만 이벤트 추가
 if (topTxt) {
-  topTxt.addEventListener("mouseenter", () => addActiveClass(topTxt));
-  topTxt.addEventListener("mouseleave", () => removeActiveClass(topTxt));
+  topTxt.addEventListener('mouseenter', () => addActiveClass(topTxt));
+  topTxt.addEventListener('mouseleave', () => removeActiveClass(topTxt));
 }
 
-// bottomTxt가 있을 때만 이벤트 추가
 if (bottomTxt) {
-  bottomTxt.addEventListener("mouseenter", () => addActiveClass(bottomTxt));
-  bottomTxt.addEventListener("mouseleave", () => removeActiveClass(bottomTxt));
+  bottomTxt.addEventListener('mouseenter', () => addActiveClass(bottomTxt));
+  bottomTxt.addEventListener('mouseleave', () => removeActiveClass(bottomTxt));
 }
 
-// 메뉴 버튼과 네비게이션 링크들 가져오기
-const navLinks = document.querySelectorAll("#m-gnb .gnb__depth1 a");
+const navLinks = document.querySelectorAll('#m-gnb .gnb__depth1 a');
 // 메뉴 버튼이 있을 때만 이벤트 추가
 if (menuBtn) {
-  menuBtn.addEventListener("click", function () {
-    toggleClass(menuBtn, "active");
+  menuBtn.addEventListener('click', function () {
+    toggleClass(menuBtn, 'active');
   });
 }
 
 // 네비게이션 링크 클릭 시 메뉴 닫기
 navLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    menuBtn.classList.remove("active");
+  link.addEventListener('click', function () {
+    menuBtn.classList.remove('active');
   });
 });
 
-// 클래스 토글 함수 정의
-function toggleClass(element, className) {
-  if (element.classList.contains(className)) {
-    element.classList.remove(className);
+function toggleClass(e, className) {
+  if (e.classList.contains(className)) {
+    e.classList.remove(className);
   } else {
-    element.classList.add(className);
+    e.classList.add(className);
   }
 }
--- resources/js/pattern/gsap.js
+++ resources/js/pattern/gsap.js
@@ -1,48 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
   // Fullpage.js 초기화
-  new fullpage("#fullpage", {
+  new fullpage('#fullpage', {
     autoScrolling: true,
     navigation: false,
     scrollOverflow: true, // 스크롤 가능한 컨텐츠 허용
     fitToSection: false,
-    anchors: ["s-intro", "s-info", "s-best", "s-news", "s-app"],
-    easingcss3: "cubic-bezier(0.76, 0, 0.24, 1)",
+    anchors: ['s-intro', 's-info', 's-best', 's-news', 's-app'],
+    easingcss3: 'cubic-bezier(0.76, 0, 0.24, 1)',
     scrollingSpeed: 1250,
     touchSensitivity: 0.01,
     onLeave: function (origin, destination, direction) {
-      const header = document.querySelector("#header");
-      if (destination.item.classList.contains("sec-album")) {
+      const header = document.querySelector('#header');
+      if (destination.item.classList.contains('sec-album')) {
         startWaveAnimation();
       }
       //  두번째 섹션을 떠날 때 헤더를 나타냄
       if (destination.index !== 1) {
-        gsap.to(header, { top: "0", duration: 0.6 });
+        gsap.to(header, { top: '0', duration: 0.6 });
       }
       ScrollTrigger.refresh();
     },
     afterLoad: (origin, destination, direction) => {
       if (destination.index === 0) {
-        gsap.to(header, { top: "-100px", duration: 0.1 });
+        gsap.to(header, { top: '-100px', duration: 0.1 });
       } else if (destination.index === 1) {
-        gsap.to(header, { top: "-100px", duration: 0.1 });
+        gsap.to(header, { top: '-100px', duration: 0.1 });
       }
 
-      history.replaceState(null, null, " ");
+      history.replaceState(null, null, ' ');
     },
   });
 
-  const bigLogo = document.querySelector(".big-logo");
-  const smallLogo = document.querySelector(".small-logo");
-  const heightBox = document.querySelector(".height_box");
+  const bigLogo = document.querySelector('.big-logo');
+  const smallLogo = document.querySelector('.small-logo');
+  const heightBox = document.querySelector('.height_box');
   const heightBoxHeight = heightBox ? heightBox.offsetHeight : window.innerHeight;
-  const introSection = document.querySelector(".intro-sec");
+  const introSection = document.querySelector('.intro-sec');
 
   ScrollTrigger.create({
     trigger: introSection,
-    start: "top top",
-    end: "bottom top",
+    start: 'top top',
+    end: 'bottom top',
     // pin: true,
     scrub: 1,
     onLeaveBack: () => ScrollTrigger.refresh(), //
@@ -51,42 +51,42 @@ document.addEventListener("DOMContentLoaded", () => {
   if (introSection && bigLogo && smallLogo) {
     // Big Logo 애니메이션 ScrollTrigger
     ScrollTrigger.create({
-      trigger: introSection.querySelector(".fp-overflow") || introSection,
-      scroller: introSection.querySelector(".fp-overflow") || introSection,
-      start: "top top",
-      end: "bottom -100%",
+      trigger: introSection.querySelector('.fp-overflow') || introSection,
+      scroller: introSection.querySelector('.fp-overflow') || introSection,
+      start: 'top top',
+      end: 'bottom -100%',
       scrub: 1,
       animation: gsap.fromTo(
         bigLogo,
-        { rotate: -400, scale: 2.5, ease: "linear" },
-        { rotate: 0, scale: 1, duration: 3, ease: "linear" }
+        { rotate: -400, scale: 2.5, ease: 'linear' },
+        { rotate: 0, scale: 1, duration: 3, ease: 'linear' }
       ),
     });
 
     // Small Logo 애니메이션 ScrollTrigger
     ScrollTrigger.create({
-      trigger: introSection.querySelector(".fp-overflow") || introSection,
-      scroller: introSection.querySelector(".fp-overflow") || introSection,
-      start: "top top",
-      end: "bottom -100%",
+      trigger: introSection.querySelector('.fp-overflow') || introSection,
+      scroller: introSection.querySelector('.fp-overflow') || introSection,
+      start: 'top top',
+      end: 'bottom -100%',
       scrub: 1,
-      animation: gsap.fromTo(smallLogo, { scale: 2.5 }, { scale: 1, duration: 3, ease: "linear" }),
+      animation: gsap.fromTo(smallLogo, { scale: 2.5 }, { scale: 1, duration: 3, ease: 'linear' }),
     });
   }
   // 클릭시 스크롤 강제 이동
   let scrollCompleted = false; // 스크롤 완료 여부
   // 스크롤 완료 여부를 체크
   const isAtBottom = () => {
-    const targetElement = document.querySelector(".fp-overflow") || window;
+    const targetElement = document.querySelector('.fp-overflow') || window;
     return targetElement.scrollY >= heightBoxHeight - window.innerHeight;
   };
-  introSection.addEventListener("click", () => {
+  introSection.addEventListener('click', () => {
     if (!scrollCompleted && !isAtBottom()) {
-      const targetElement = document.querySelector(".fp-overflow") || window;
+      const targetElement = document.querySelector('.fp-overflow') || window;
       gsap.to(targetElement, {
         scrollTo: { y: heightBoxHeight, autoKill: true },
         duration: 1.5,
-        ease: "power2.out",
+        ease: 'power2.out',
         onComplete: () => {
           scrollCompleted = true; // 스크롤 완료 상태로 변경
         },
@@ -97,57 +97,55 @@ document.addEventListener("DOMContentLoaded", () => {
       scrollCompleted = false;
     }
   });
-  // video scroll event
-  const video = document.querySelector(".video-scale");
-  const secScroller = document.querySelector(".sec-01 .fp-overflow");
-  const sectionVideo = document.querySelector(".movie-item");
+
+  const video = document.querySelector('.video-scale');
+  const secScroller = document.querySelector('.sec-01 .fp-overflow');
+  const sectionVideo = document.querySelector('.movie-item');
 
   // 비디오 초기 상태 설정
   gsap.set(video, {
     scale: 0.35,
-    y: "65%",
+    y: '65%',
     borderRadius: 35,
   });
   ScrollTrigger.create({
-    trigger: sectionVideo, // 트리거할 요소
-    scroller: secScroller, // 스크롤러로 사용할 요소
-    start: "top 50%",
-    end: "bottom bottom",
+    trigger: sectionVideo,
+    scroller: secScroller,
+    start: 'top 50%',
+    end: 'bottom bottom',
     scrub: 4,
     markers: false,
-    // 애니메이션 설정
     animation: gsap.fromTo(
       video,
       {
         scale: 0.35,
-        y: "65%",
+        y: '65%',
         borderRadius: 35,
       },
       {
         y: 0,
         scale: 1,
         borderRadius: 0,
-        ease: "cubic-bezier(0.45, 0, 0.55, 1)", // 커스텀 이징
+        ease: 'cubic-bezier(0.45, 0, 0.55, 1)',
         duration: 1.5,
       }
     ),
   });
 
   // 모든 앨범 아이템 배열화
-  const albumItems = gsap.utils.toArray(".album-item");
-  const albumS = document.querySelector(".sec-album .fp-overflow");
-  const albumBg = document.querySelector(".album-bg");
+  const albumItems = gsap.utils.toArray('.album-item');
+  const albumS = document.querySelector('.sec-album .fp-overflow');
+  const albumBg = document.querySelector('.album-bg');
   let previousSrc = [albumBg.style.backgroundImage];
-  // 각 앨범 아이템에 대해 반복문 실행
   albumItems.forEach((item, index) => {
-    const img = item.querySelector(".album-cover");
-    const imgSrc = img.getAttribute("src");
+    const img = item.querySelector('.album-cover');
+    const imgSrc = img.getAttribute('src');
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: item, // 앨범 리스트가 트리거
         scroller: albumS,
-        start: "top 90%", // 앨범 리스트가 80%에 도달할 때 시작
-        end: "top 70%", // 앨범 리스트가 20%에 도달할 때 종료
+        start: 'top 90%',
+        end: 'top 70%',
         scrub: 2,
         onEnter: () => {
           // 새로운 배경 이미지 설정
@@ -169,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
         onLeave: () => {
           // 스크롤이 아래로 갈 때 다음 이미지 복원
           if (previousSrc[index + 1]) {
-            albumBg.style.backgroundImage = previousSrc[index + 1] || "none"; // 다음 이미지로 복원
+            albumBg.style.backgroundImage = previousSrc[index + 1] || 'none'; // 다음 이미지로 복원
           }
         },
       },
@@ -182,26 +180,26 @@ document.addEventListener("DOMContentLoaded", () => {
         y: 0,
         scale: 1,
         duration: 2, // 부드러운 애니메이션 시간
-        ease: "power2.out", // 천천히 멈추는 느낌
+        ease: 'power2.out', // 천천히 멈추는 느낌
       }
     ).to(item, {
       scale: 1.1,
       duration: 1, // 잠시 커지는 느낌 주기
-      ease: "linear",
+      ease: 'linear',
       repeat: 2,
       yoyo: true,
     });
   });
 
   // wave 애니메이션
-  const waveItems = gsap.utils.toArray(".waves > li");
+  const waveItems = gsap.utils.toArray('.waves > li');
 
   // ScrollTrigger로 album 섹션에 진입할 때 애니메이션 작동
   ScrollTrigger.create({
     trigger: albumS,
-    start: "top 100%",
-    end: "bottom bottom",
-    onEnter: () => startWaveAnimation(), // 진입 시 애니메이션 시작
+    start: 'top 100%',
+    end: 'bottom bottom',
+    onEnter: () => startWaveAnimation(),
   });
 
   // 웨이브 애니메이션 함수
@@ -217,75 +215,75 @@ document.addEventListener("DOMContentLoaded", () => {
         duration: randomDuration,
         repeat: -1,
         yoyo: true,
-        ease: "sine.inOut",
+        ease: 'sine.inOut',
         delay: index * 0.1 + randomDelay,
       });
     });
   }
 
-  const scrolling = document.querySelector(".app-player .fp-overflow");
-  const scrollBox = document.querySelector(".scroll_box");
-  const rotateBox = document.querySelector(".rotate_box");
+  const scrolling = document.querySelector('.app-player .fp-overflow');
+  const scrollBox = document.querySelector('.scroll_box');
+  const rotateBox = document.querySelector('.rotate_box');
   // gsap.to(rotateBox, {
-  //   rotation: 360, // 360도 회전
-  //   duration: 70, // 회전 애니메이션 지속 시간
+  //   rotation: 360,
+  //   duration: 70,
   //   repeat: -1, // 무한 반복
-  //   ease: "none", // 매끄럽게 회전
+  //   ease: "none",
   // });
 
   const tl2 = gsap.timeline();
-  tl2.addLabel("scroll");
+  tl2.addLabel('scroll');
   tl2
-    .set(".t__dep1", { opacity: 1, scale: 1 })
+    .set('.t__dep1', { opacity: 1, scale: 1 })
     .fromTo(
-      ".circle_box",
+      '.circle_box',
       { scale: 1, opacity: 1, rotation: 0 },
       { scale: 0, opacity: 0, rotation: 180, duration: 7 },
-      ">1"
+      '>1'
     )
-    .to(".t__dep1", { opacity: 0, scale: 0, duration: 4 }, "<")
-    .set(".circle_small", { opacity: 0, scale: 0 }, "<")
-    .to(".circle_small", { opacity: 1, scale: 1, duration: 6 }, ">1")
-    .set(".t__dep2", { opacity: 0, scale: 0 }, "<")
-    .to(".t__dep2", { opacity: 1, scale: 1, duration: 6 }, "<")
-    .fromTo(".circle_small .circle img", { rotation: 340 }, { rotation: 360, duration: 4 }, ">1")
-    .to(".item__img", { opacity: 1, duration: 2 }, ">1.5")
-    .to(".item__img", { opacity: 0, duration: 7 }, ">1.5")
-    .to(".t__dep2", { opacity: 0, scale: 0, duration: 2 }, ">2")
-    .fromTo(".circle_small .circle img", { rotation: 360, scale: 1 }, { rotation: 400, scale: 0.7, duration: 7 }, ">")
-    .set(".circle_large", { opacity: 0, scale: 0 }, "<")
-    .to(".circle_large", { opacity: 1, scale: 1, duration: 4 }, "<")
-    .set(".t__dep3", { scale: 0 }, "<")
-    .to(".t__dep3", { opacity: 1, scale: 1, duration: 8 }, "<")
-    .to(".t__dep3", { opacity: 0, scale: 0, duration: 4 }, ">3")
-    .to(".circle_large", { scale: 1.5, opacity: 1, rotation: 30, duration: 10 }, ">1")
-    .to(".circle_small", { scale: 1.27, opacity: 1, rotation: 50, duration: 10 }, "<")
-    .to(".t__dep4", { opacity: 1, scale: 1, duration: 12 }, "<1")
-    .fromTo(".video_item", { opacity: 0 }, { opacity: 1, duration: 5 }, ">")
-    .to(".t__dep4", { opacity: 0, scale: 0, duration: 7 }, ">3")
-    .to(".circle_small", { opacity: 0, scale: 0, duration: 20 }, "<")
-    .to(".circle_large", { scale: 0, opacity: 0, rotation: 0, duration: 25 }, "<")
-    .to(".video_txt", { opacity: 1, duration: 10 }, "<")
-    .fromTo(".bg_dim", { opacity: 1, scale: 0, duration: 12 }, { opacity: 0.8, scale: 8, duration: 12 }, ">1.5")
-    .set(".app_logo", { y: -50 }, "<")
-    .set(".app_download", { y: 50 }, "<")
-    .to(".app_logo", { opacity: 1, duration: 8, y: 0 }, ">")
-    .to(".app_download", { opacity: 1, duration: 8, y: 0 }, "<")
-    .to("#footer", { bottom: "0", duration: 10 });
+    .to('.t__dep1', { opacity: 0, scale: 0, duration: 4 }, '<')
+    .set('.circle_small', { opacity: 0, scale: 0 }, '<')
+    .to('.circle_small', { opacity: 1, scale: 1, duration: 6 }, '>1')
+    .set('.t__dep2', { opacity: 0, scale: 0 }, '<')
+    .to('.t__dep2', { opacity: 1, scale: 1, duration: 6 }, '<')
+    .fromTo('.circle_small .circle img', { rotation: 340 }, { rotation: 360, duration: 4 }, '>1')
+    .to('.item__img', { opacity: 1, duration: 2 }, '>1.5')
+    .to('.item__img', { opacity: 0, duration: 7 }, '>1.5')
+    .to('.t__dep2', { opacity: 0, scale: 0, duration: 2 }, '>2')
+    .fromTo('.circle_small .circle img', { rotation: 360, scale: 1 }, { rotation: 400, scale: 0.7, duration: 7 }, '>')
+    .set('.circle_large', { opacity: 0, scale: 0 }, '<')
+    .to('.circle_large', { opacity: 1, scale: 1, duration: 4 }, '<')
+    .set('.t__dep3', { scale: 0 }, '<')
+    .to('.t__dep3', { opacity: 1, scale: 1, duration: 8 }, '<')
+    .to('.t__dep3', { opacity: 0, scale: 0, duration: 4 }, '>3')
+    .to('.circle_large', { scale: 1.5, opacity: 1, rotation: 30, duration: 10 }, '>1')
+    .to('.circle_small', { scale: 1.27, opacity: 1, rotation: 50, duration: 10 }, '<')
+    .to('.t__dep4', { opacity: 1, scale: 1, duration: 12 }, '<1')
+    .fromTo('.video_item', { opacity: 0 }, { opacity: 1, duration: 5 }, '>')
+    .to('.t__dep4', { opacity: 0, scale: 0, duration: 7 }, '>3')
+    .to('.circle_small', { opacity: 0, scale: 0, duration: 20 }, '<')
+    .to('.circle_large', { scale: 0, opacity: 0, rotation: 0, duration: 25 }, '<')
+    .to('.video_txt', { opacity: 1, duration: 10 }, '<')
+    .fromTo('.bg_dim', { opacity: 1, scale: 0, duration: 12 }, { opacity: 0.8, scale: 8, duration: 12 }, '>1.5')
+    .set('.app_logo', { y: -50 }, '<')
+    .set('.app_download', { y: 50 }, '<')
+    .to('.app_logo', { opacity: 1, duration: 8, y: 0 }, '>')
+    .to('.app_download', { opacity: 1, duration: 8, y: 0 }, '<')
+    .to('#footer', { bottom: '0', duration: 10 });
 
   ScrollTrigger.create({
     animation: tl2,
     scroller: scrolling,
     trigger: scrollBox,
-    start: "top top",
-    end: "bottom -200%",
+    start: 'top top',
+    end: 'bottom -200%',
     scrub: 2.5,
-    ease: "linear",
+    ease: 'linear',
   });
 
-  window.addEventListener("resize", () => {
-    const introSection = document.querySelector(".intro-sec");
-    const newsSection = document.querySelector(".s-news");
+  window.addEventListener('resize', () => {
+    const introSection = document.querySelector('.intro-sec');
+    const newsSection = document.querySelector('.s-news');
     introSection.style.height = `${window.innerHeight}px`;
     newsSection.style.height = `${window.innerHeight}px`;
     ScrollTrigger.refresh();
